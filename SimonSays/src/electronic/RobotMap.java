9fea734d3b1440ae1b6f1d4b5810eac35bb1bbca
omri <omri@DESKTOP-GDC53VD>
2018-11-18 12:57:12 +0200

    adde robot map

---
+++ SimonSays/src/electronic/RobotMap.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package electronic;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	public interface SIMON{
+		public static final int[] LIGHTS = {0,1,2,3};
+		public static final int[] BUTTONS = {3,4,5,6};
+	}
+}
