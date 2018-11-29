0dcba6cbad5aecee594d524c1c39a4da9da5691f
omri <omri@DESKTOP-GDC53VD>
2018-11-29 12:11:19 +0200

    docs

--- SimonSays/src/grafics/Menu.java
+++ SimonSays/src/grafics/Menu.java
@@ -14,31 +14,40 @@ import javax.swing.JRadioButton;
 
 import logics.Calculator.Difficulty;
 
+/**
+ * This class is the menu that let you pick difficulty, set to inverted mode,
+ * see the score board, read the instructions or exit the game.
+ * 
+ * @author omri
+ *
+ */
 public class Menu extends JPanel {
 	JFrame frame;
 	JRadioButton easy;
 	JRadioButton medium;
 	JRadioButton hard;
+	JButton playButton;
+	JButton exitButton;
+	JButton helpButton;
+	JButton scoreButton;
+	JCheckBox invertedButton;
+	ButtonGroup difficultyGroup;
+	JPanel settingsPanel;
 
+	/**
+	 * Makes a new menu.
+	 */
 	public Menu() {
-		JButton playButton = new JButton("START!");
-		JButton exitButton = new JButton("Exit Game");
-		JButton helpButton = new JButton("Help");
-		JButton scoreButton = new JButton("Score Board");
-		JCheckBox invertedButton = new JCheckBox("Inverted");
-		JPanel settingsPanel = new JPanel();
-		easy = new JRadioButton("easy", false);
-		medium = new JRadioButton("medium", true);
-		hard = new JRadioButton("hard", false);
-		ButtonGroup difficultyGroup = new ButtonGroup();
+		initButtons();
+		initSettings();
 
-		difficultyGroup.add(easy);
-		difficultyGroup.add(medium);
-		difficultyGroup.add(hard);
-		settingsPanel.add(invertedButton);
-		settingsPanel.add(easy);
-		settingsPanel.add(medium);
-		settingsPanel.add(hard);
+		this.setLayout(new GridLayout(5, 1));
+		this.add(playButton);
+		this.add(settingsPanel);
+		this.add(scoreButton);
+		this.add(helpButton);
+		this.add(exitButton);
+		initFrame();
 
 		playButton.addActionListener((ActionEvent e) -> {
 			GraphicBoard board = new GraphicBoard(invertedButton.isSelected(), getDifficulty());
@@ -49,21 +58,55 @@ public class Menu extends JPanel {
 		exitButton.addActionListener((ActionEvent e) -> {
 			frame.dispose();
 		});
+	}
+
+	/**
+	 * Make the new frame and add the menu to it.
+	 */
+	private void initFrame() {
 		frame = new JFrame("Simon");
 		frame.setBounds(0, 0, 500, 500);
 		frame.setMinimumSize(new Dimension(300, 300));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		this.setLayout(new GridLayout(5, 1));
-		this.add(playButton);
-		this.add(settingsPanel);
-		this.add(scoreButton);
-		this.add(helpButton);
-		this.add(exitButton);
 		frame.add(this);
 		frame.add(new GraphicBoard(false, Difficulty.EASY));
 	}
 
+	/**
+	 * make all of the button.
+	 */
+	private void initButtons() {
+		playButton = new JButton("START!");
+		exitButton = new JButton("Exit Game");
+		helpButton = new JButton("Help");
+		scoreButton = new JButton("Score Board");
+		invertedButton = new JCheckBox("Inverted");
+		easy = new JRadioButton("easy", false);
+		medium = new JRadioButton("medium", true);
+		hard = new JRadioButton("hard", false);
+
+		difficultyGroup = new ButtonGroup();
+
+		difficultyGroup.add(easy);
+		difficultyGroup.add(medium);
+		difficultyGroup.add(hard);
+	}
+
+	/**
+	 * Add the setting related button to a new panel.
+	 */
+	private void initSettings() {
+		settingsPanel = new JPanel();
+		settingsPanel.add(invertedButton);
+		settingsPanel.add(easy);
+		settingsPanel.add(medium);
+		settingsPanel.add(hard);
+	}
+
+	/**
+	 * @return the difficulty.
+	 */
 	public Difficulty getDifficulty() {
 		if (easy.isSelected())
 			return Difficulty.EASY;
@@ -72,6 +115,9 @@ public class Menu extends JPanel {
 		return Difficulty.MEDIUM;
 	}
 
+	/**
+	 * Start the game.
+	 */
 	public static void main(String[] args) {
 		new Menu();
 	}
